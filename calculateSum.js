1927eba5505f79d59364f29bdd048ef6eb468044
Dipendra.Shekhawat <dipendra.shekhawat07@gmail.com>
2019-08-11 18:43:10 +0530

    added validation test cases

--- calculateSum.js
+++ calculateSum.js
@@ -1,3 +1,10 @@
 export function calculateSum(a,b){
     return a+b;
 }
+
+export const validateNumbers = (a, b) => {
+    if (isNaN(a) && isNaN(b)) {
+      return false;
+    }
+    return true;
+  }
--- calculateSum.test.js
+++ calculateSum.test.js
@@ -1,4 +1,4 @@
-import {calculateSum} from './calculateSum';
+import {calculateSum, validateNumbers} from './calculateSum';
 
 // passing test case
 test('adds 10 + 20 to equal 30', () => {
@@ -8,4 +8,17 @@ test('adds 10 + 20 to equal 30', () => {
 // failing test case
 test('adds 2 + 2 to equal 5', () => {
     expect(calculateSum(2,2)).toBe(5);
+});
+
+// Adding validation test cases
+test('string is used instead of a number', () => {
+    expect(validateNumbers('ds', 5)).not.toBeTruthy();
+  });
+
+  test('both the inputs are of type string instead of number', () => {
+      expect(validateNumbers('Dipendra', 'Shekhawat')).toBeTruthy();
+  });
+
+  test('validation successful as both the inputs are numbers', () => {
+    expect(validateNumbers(25, 25)).toBeTruthy();
 });
